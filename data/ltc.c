d2dd1b262cfc35460cad38efb665cbad853c548a
Louis Legrand <l.legrand@openium.fr>
2022-09-30 08:18:49 +0200

    Add lesson implementation

---
+++ data/ltc.c
@@ -0,0 +1,5 @@
+//
+// Created by Louis Legrand on 28/09/2022.
+//
+
+#include "ltc.h"
---
+++ data/ltc.h
@@ -0,0 +1,8 @@
+//
+// Created by Louis Legrand on 28/09/2022.
+//
+
+#ifndef GRANDELIA_LTC_H
+#define GRANDELIA_LTC_H
+
+#endif //GRANDELIA_LTC_H
